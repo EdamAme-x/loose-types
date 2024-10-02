db5da0a77348585fb80f497d0e4a33a3b052dc13
EdamAme-x <edame8080@gmail.com>
2024-10-02 12:58:48 +0900

    update

--- mod.ts
+++ mod.ts
@@ -11,4 +11,6 @@ export type LooseArray = LooseType[];
 // deno-lint-ignore ban-types
 export type LooseObject<Must extends Record<PropertyKey, LooseType> = {}> = Record<PropertyKey, LooseType> & Must;
 
-export type LooseFunction = (...args: LooseType[]) => LooseType;
+export type LooseFunction<Args extends LooseType[] = LooseType[], Ret extends LooseType = LooseType> = (...args: Args) => Ret;
+
+export type LoosePrimitive = string | number | boolean | null | undefined;
